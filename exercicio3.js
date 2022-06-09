30e229e12d4c9892b457167c2aa8fccab8d243fc
Erickson Dias <103853894+EricksonLOOP@users.noreply.github.com>
2022-06-09 00:05:40 -0300

    Update exercicio3.js

--- exercicio3.js
+++ exercicio3.js
@@ -7,18 +7,18 @@ function somar(numero1, numero2) {
 
 function subtrair(numero1, numero2) {
     let subtrair = 30 - 20;
-    return 'A soma dos dois números é: ' + subtrair
+    return 'A subtração dos dois números é: ' + subtrair
 }
 
 function multiplo(numero1, numero2) {
     let multiplo = 30 * 20;
-    return 'A soma dos dois números é: ' + multiplo;
+    return 'A multiplicação dos dois números é: ' + multiplo;
 
 }
 
 function divisor(numero1, numero2) {
     let divisor = 30 / 20;
-    return 'A soma dos dois números é: ' + divisor;
+    return 'A divisão dos dois números é: ' + divisor;
 
 }
 console.log(somar())
