573faef226de6e7db862cc4a1aada6d05e0ef56f
Erickson Dias <103853894+EricksonLOOP@users.noreply.github.com>
2022-06-08 23:55:15 -0300

    Add files via upload

---
+++ exercicio3.js
@@ -0,0 +1,27 @@
+
+function somar(numero1, numero2) {
+    let soma = 30 + 20;
+    return 'A soma dos dois números é: ' + soma;
+
+}
+
+function subtrair(numero1, numero2) {
+    let subtrair = 30 - 20;
+    return 'A soma dos dois números é: ' + subtrair
+}
+
+function multiplo(numero1, numero2) {
+    let multiplo = 30 * 20;
+    return 'A soma dos dois números é: ' + multiplo;
+
+}
+
+function disivisor(numero1, numero2) {
+    let disivisor = 30 / 20;
+    return 'A soma dos dois números é: ' + disivisor;
+
+}
+console.log(somar())
+console.log(subtrair())
+console.log(multiplo())
+console.log(disivisor())
